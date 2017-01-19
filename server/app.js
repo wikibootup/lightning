801636b3be85cac62ed170738b77b93f2aa433d7
wikibootup <wikibootup@gmail.com>
2017-01-19 12:15:12 +0900

    register auth router

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,9 +20,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
-app.use('/users', users);
+app.use('/', require('./auth/router'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
