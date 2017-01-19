6ef3dae32bbb09756922c7658659fc8b4a4014ee
wikibootup <wikibootup@gmail.com>
2017-01-19 15:08:51 +0900

    route oauth2 login T+

--- server/auth/router.js
+++ server/auth/router.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/login', function(req, res, next) {
-  res.send('login');
+var Auth = require('./auth');
+auth = new Auth();
+
+router.get('/auth/thingplus', function(req, res, next) {
+  passport.use
+//  res.redirect(auth.getAuthUrl());
 });
 
 module.exports = router;
