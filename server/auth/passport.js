5bf804ffb4b5f9f237f614d440ee8c6dde838a6d
wikibootup <wikibootup@gmail.com>
2017-01-20 18:10:41 +0900

    update auth: full passport auth for T+

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,13 +1,17 @@
 var passport = require('passport')
   , OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var request = require('request');
+var Options = require('../api/options');
 
 function Passport() {
   this.passport = passport;
+  this.scope = { scope: ["user-profile-read", "gateway-update", "timeline-read", 
+    "tag", "rule-read", "service-read", "site-read"] };
 }
 
 Passport.prototype.configureSession = function() {
   this.passport.serializeUser(function(user, cb) {
-    cb(null, 'test serialized user');
+    cb(null, user);
   });
   
   this.passport.deserializeUser(function(id, cb) {
@@ -27,12 +31,26 @@ Passport.prototype.configureThingplusStrategy = function(name) {
       tokenURL: process.env['TOKEN_URL'],
       clientID: process.env['CLIENT_ID'],
       clientSecret: process.env['CLIENT_SECRET'],
-      callbackURL: process.env['CALLBACK_URL']
+      callbackURL: process.env['CALLBACK_URL'],
     },
     function(accessToken, refreshToken, profile, cb) {
-      console.log('access token=');
-      console.log(accessToken);
-      cb(null,profile);      
+      const userData = 
+        request(
+          Options.getThingplusOptions(
+            method='GET', resource_url='/users/me', token=accessToken
+          ), 
+          function (error, response, body) {
+            if (error) throw new Error(error);
+          }
+        );
+      
+      profile = {
+        id: userData.id,
+        name: userData.loginId,
+        accessToken: accessToken
+      }
+
+      cb(null, profile);
     }
   ));
 };
