a1305d4995070bdb28f7ec0e66e518bc40aa4712
wikibootup <wikibootup@gmail.com>
2017-01-20 12:30:48 +0900

    update auth router : callback using passport

--- server/auth/router.js
+++ server/auth/router.js
@@ -8,8 +8,12 @@ auth = new Auth();
 router.get('/auth/thingplus',
   passport.authenticate('thingplus'));
 
-router.get('/auth/thingplus/callback', function(req, res, next) {
-  res.send('callback');
-});
+router.get('/auth/thingplus/callback',
+  passport.authenticate('thingplus', { failureRedirect: '/auth/thingplus/error' }),
+    function(req, res) {
+      console.log(req.url.query);
+      res.redirect('/');
+    }
+);
 
 module.exports = router;
