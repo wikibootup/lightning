84a4795d51137b3e4777b7db78cf3364fb7807ba
wikibootup <wikibootup@gmail.com>
2017-01-20 12:30:21 +0900

    update passport : make object that has configure methods

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,23 +1,40 @@
 var passport = require('passport')
   , OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 
+function Passport() {
+  this.passport = passport;
+}
+
+Passport.prototype.configureSession = function() {
+  this.passport.serializeUser(function(user, cb) {
+    cb(null, 'test serialized user');
+  });
+  
+  this.passport.deserializeUser(function(id, cb) {
+    User.findById(id, function(err, user) {
+      cb(err, user);
+    });
+  });
+}
+ 
 /**
 * Configure OAuth2 authorization for Thingplus
 * @param {String} name - Strategy name
 */
-function configureThingplusStrategy(name) {
+Passport.prototype.configureThingplusStrategy = function(name) {
   passport.use(name, new OAuth2Strategy({
-    authorizationURL: process.env['AUTHORIZATION_URL'],
-    tokenURL: process.env['TOKEN_URL'],
-    clientID: process.env['CLIENT_ID'],
-    clientSecret: process.env['CLIENT_SECRET'],
-    callbackURL: process.env['CALLBACK_URL']
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log(profile);
-    done(err, profile);
+      authorizationURL: process.env['AUTHORIZATION_URL'],
+      tokenURL: process.env['TOKEN_URL'],
+      clientID: process.env['CLIENT_ID'],
+      clientSecret: process.env['CLIENT_SECRET'],
+      callbackURL: process.env['CALLBACK_URL']
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log('access token=');
+      console.log(accessToken);
+      cb(null,profile);      
     }
   ));
-}
+};
 
-exports.configureThingplusStrategy = configureThingplusStrategy;
+module.exports = Passport;
