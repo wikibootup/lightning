f80f97917dce3f96c8fcc779e2f3035ad6121f1a
wikibootup <wikibootup@gmail.com>
2017-01-23 16:45:13 +0900

    add http get type with inteface(IUser)

--- client/src/app/auth/shared/auth.service.ts
+++ client/src/app/auth/shared/auth.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { IUser } from './user'; 
 
 @Injectable()
 export class AuthService {
 
   isLoggedIn: boolean = false;
-  redirectUrl: string;
 
-  constructor(private _http: Http) {
-    if(this.getAuth()) {
-      this.isLoggedIn = true;
-    }
-  }
-
-  getAuth() {
+  constructor(private _http: Http) { }
+  
+  getAuth(): Observable<IUser> {
     return this._http.get('/api/auth')
       .map((res: Response) => res.json());
   }
