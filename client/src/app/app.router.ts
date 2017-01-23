6ae05932c56bfc4a6ae4772723e379fe74925095
wikibootup <wikibootup@gmail.com>
2017-01-23 17:22:30 +0900

    init home in pages

--- client/src/app/app.router.ts
+++ client/src/app/app.router.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { usersRoutes } from './users/users.router';
+import { HomeComponent } from './pages/home/index';
 
 export const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
 ];
 
 export const AppRouter = RouterModule.forRoot(appRoutes);
--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { PagesRouter } from './pages.router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PagesRouter
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, HomeComponent]
 })
 export class PagesModule { }
