72dceab810586255e17ca3e5d94b68e5a2a7258b
wikibootup <wikibootup@gmail.com>
2017-01-24 20:19:11 +0900

    route gateways

--- server/api/router.js
+++ server/api/router.js
@@ -3,5 +3,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/auth', require('./auth/router'));
+router.use('/gateways', require('./gateways/router'));
 
 module.exports = router;
