846d54f40a47bf179a053da7ae5eecc95c396350
wikibootup <wikibootup@gmail.com>
2017-01-31 19:14:31 +0900

    apply sample name validator to new building

--- server/api/buildings/router.js
+++ server/api/buildings/router.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var util = require('util')
+
 var Buildings = require('./buildings');
 
 var router = express.Router();
@@ -8,7 +10,15 @@ router.get('/', function(req, res) {
 });
 
 router.post('/new', function(req, res) {
+  req.checkBody("name", "Enter a valid name.").notEmpty();
+
+  req.getValidationResult().then(function(result) {
+    if (!result.isEmpty()) {
+      res.status(400).send('There have been validation errors: ' + util.inspect(result.array()));
+      return;
+    }
   res.json(Buildings.register(req.body));
+  });
 });
 
 module.exports = router;
