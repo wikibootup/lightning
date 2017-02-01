1aa04d2ac39c09b9f7372468f18794c50efde748
wikibootup <wikibootup@gmail.com>
2017-02-01 17:30:28 +0900

    update dashboard : http building get, post by event

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpGatewaysService } from '../../core/http/http-gateways.service';
+import { HttpBuildingsService } from '../../core/http/http-buildings.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +11,27 @@ export class DashboardComponent implements OnInit {
 
   private gateways: string;
 
-  constructor(private _httpGatewaysService: HttpGatewaysService) { }
+  constructor(private _httpGatewaysService: HttpGatewaysService,
+              private _httpBuildingsService: HttpBuildingsService) { }
 
   ngOnInit() {
     this._httpGatewaysService.getGateways()
       .subscribe(
         data => { this.gateways = data; }
+      )
+  }
+
+  getBuildings(): void {
+    this._httpBuildingsService.getBuildings()
+      .subscribe(
+        data => { console.log(data, 'clicked getBuildings'); }
+      )
+  }
+  
+  newBuilding(): void {
+    this._httpBuildingsService.newBuilding()
+      .subscribe(
+        data => { console.log(data, 'clicked new Building'); }
       )
   }
 
